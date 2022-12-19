f3246dbd901690ffc6e12a3ec3db0c90bd515cba
rajchelm <49623773+rajchelm@users.noreply.github.com>
2022-12-19 01:00:39 +0100

    9-12

--- scratch.py
+++ scratch.py
@@ -1,13 +1,17 @@
 import requests
+import json
 
-response = requests.get("http://api.nbp.pl/api/exchangerates/tables/a")#.json()
-if response.ok:
-        print(response)
-        print(response.ok)
-        data = response.json()
-        # rates = data["rates"]
-        # base = data["table"]
-        # date = data["effectiveDate"]
-        # print("base: " + base)
-        # print("date: " + date)
-print(data)
+nbp = requests.get("http://api.nbp.pl/api/exchangerates/tables/a").json()
+
+x = nbp[0]
+print("x - wartość zmiennej x: ", x)
+print()
+
+y = x["rates"]
+print("y - Wartość klucza rates: ", y)
+
+print()
+for curr in y:
+    print()
+    for key, value in curr.items():
+        print(key + ": ", value)
